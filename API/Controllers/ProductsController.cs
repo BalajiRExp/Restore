818165de629a3b3e9c309d98cb33dba7ea85eb48
TP003 Venkata Satish G <tp003@himalayawellness.com>
2022-07-09 15:59:22 +0530

    End Of Secion_2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        private readonly StoreContext _context;
+        
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        
+        }
+        [HttpGet]
+        // public ActionResult<List<Product>> GetProduct()
+        // {
+        //     var products=context.Products.ToList();
+        //     return Ok(products);
+        // }
+        // [HttpGet("{id}")]
+        // public ActionResult<Product> GetProduct(int id)
+        // { 
+        //     return context.Products.Find(id);
+        // }
+
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            
+            return await _context.Products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async  Task<ActionResult<Product>> GetProduct(int id)
+        { 
+            return await _context.Products.FindAsync(id);
+        }
+
+        
+    }
+}
